b829d5c0575a812d7fcf8258a7f51331bc6c0284
anttsax-cryptos <anttsax@gmail.com>
2026-05-25 16:07:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -162,8 +162,6 @@ if user_input := st.chat_input("Γράψτε το μήνυμά σας εδώ..."
         except Exception:
             pass
      return "\n\n".join(context_list) if context_list else None
-
-
 # --- CONFIGURATION ΣΕΛΙΔΑΣ ---
 st.set_page_config(page_title="StrictexAI v2", layout="wide", page_icon="🤖")
 st.title("🤖 StrictexAI ChatBot")
